637c7134135c0d014988ec8e3e3af65da1f82052
richietan2018 <54669372+richietan2018@users.noreply.github.com>
2024-10-25 23:52:35 +0800

    Update Commit- V3.3
    25102024_3
    Update
    --
    - Modify Methodology page

--- pages/6_Methodology.py
+++ pages/6_Methodology.py
@@ -10,13 +10,13 @@ st.set_page_config(
 st.title("Methodology")
 
 st.subheader("Use Case 1: Simple enquiry on process of purchasing resale flat")
-st.image("images/UseCase1.jpg", caption="Use Case 1 Methodology")
+st.image("images/UseCase1.jpg", caption="Use Case 1 Methodology: LLM")
 st.divider()
 
 st.subheader("**Use Case 2:** Enquiry on resale flat pricing and records")
-st.image("images/UseCase2.jpg", caption="Use Case 2 Methodology")
+st.image("images/UseCase2.jpg", caption="Use Case 2 Methodology : LLM")
 st.divider()
 
 st.subheader("**Use Case 3:** Requirements and Recommendation")
-st.image("images/UseCase3.jpg", caption="Use Case 3 Methodology")
+st.image("images/UseCase3.jpg", caption="Use Case 3 Methodology: Multi Agent Systems with Crew AI")
 st.divider()
